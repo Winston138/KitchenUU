17b2f8458ec6ab789d84f1699af65c5516dd681c
mtheoz <theozzz@yandex.ru>
2021-03-31 18:50:48 +0800

    Запилил уведомление об обработке персональных данных + мелкие доработки

--- site.js
+++ site.js
@@ -1,3 +1,20 @@
 $( document ).ready(function() {
     $("#phone").mask("+7 (999) 999-9999");
+
+    $(".pdn__open").click(function () {
+        $(".pdn").toggle();
+    })
+
+    $(".pdn__close").click(function () {
+        $(".pdn").hide();
+    })
+
+    $(document).mouseup(function (e){
+        var div = $(".pdn");
+        if (!div.is(e.target)
+            && div.has(e.target).length === 0) {
+            div.hide();
+        }
+    });
+
 });
